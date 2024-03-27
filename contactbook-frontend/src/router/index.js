a4fb34e3395b5cd7c1c6f3afe016af737d4de6ee
NguyenThanhNghia-B2014589 <nghiakjg234@gmail.com>
2024-03-27 20:29:25 +0700

    Tao trang them moi lien he

--- contactbook-frontend/src/router/index.js
+++ contactbook-frontend/src/router/index.js
@@ -22,6 +22,12 @@ const routes = [
         props: true // Truyền các biến trong $route.params vào làm props
     },
 
+    {
+        path: "/contacts/add",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
+    },
+
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
